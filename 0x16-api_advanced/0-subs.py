6ea5fcc237f283c3bdcae3c0ca900edfc5d8916b
Vinsmoke37 <erraoui.1937@gmail.com>
2024-08-16 00:55:10 +0100

    0-sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ print("Hello")
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers)
-    print(response.json().get("data"))
     if response.status_code != 200:
         return 0
     return response.json().get("data").get("subscribers")
+
+
