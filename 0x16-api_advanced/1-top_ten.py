0d5b83ca25c93ad7ff72f833c6365e11810ca24d
Vinsmoke37 <erraoui.1937@gmail.com>
2024-08-16 01:15:40 +0100

    tenten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,14 +4,14 @@ import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
-
 def top_ten(subreddit):
-    """top ten hot post on a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
-        print("None")
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(None)
+        return
+    
+    data = response.json().get("data")
+    children = data.get("children")
+    for child in children:
+        print(child.get("data").get("title"))
