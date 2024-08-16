3b9b20e17fe36b9e9139cb0f7fa008b418acae3b
Vinsmoke37 <erraoui.1937@gmail.com>
2024-08-16 01:11:10 +0100

    tenten

--- 1-top_ten.py
+++ 1-top_ten.py
@@ -4,12 +4,14 @@ import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
+
 def top_ten(subreddit):
+    """top ten hot post on a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        print(None)
-        return       
-    data = response.json()
-    for post in data["data"]["children"]:
-        print(post["data"]["title"])
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
